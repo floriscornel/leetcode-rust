1353468d436d4a865ae0fde4f4178a500986a1a4
Floris Cornel <floris@cornel.email>
2022-03-20 16:34:31 +0900

    migrating to greedy

--- src/problems/minimum-domino-rotations-for-equal-row.rs
+++ src/problems/minimum-domino-rotations-for-equal-row.rs
@@ -49,71 +49,109 @@ struct Solution {}
 // @leetup=inject:before_code_ex
 
 // @leetup=code
-use std::collections::{HashSet, VecDeque};
-#[derive(Debug, Clone)]
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet};
+
+#[derive(Clone, Eq, PartialEq)]
 struct State {
-    tops: Vec<i32>,
-    bottoms: Vec<i32>,
-    flipped: HashSet<usize>,
+    cost: usize,
+    moves: HashSet<usize>,
+}
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Solution {
-    fn flip(current: &State, choice: usize) -> State {
-        let mut new = State {
-            tops: current.tops.clone(),
-            bottoms: current.bottoms.clone(),
-            flipped: current.flipped.clone(),
+    pub fn min_domino_rotations(tops: Vec<i32>, bottoms: Vec<i32>) -> i32 {
+        let number_of_choices = tops.len();
+        let mut states = BinaryHeap::new();
+        let mut initial_state = State {
+            cost: 0,
+            moves: HashSet::<usize>::new(),
         };
-        new.tops[choice] = current.bottoms[choice];
-        new.bottoms[choice] = current.tops[choice];
-        new.flipped.insert(choice);
-        new
+        if Solution::completed(&initial_state, &tops, &bottoms) {
+            return 0;
+        }
+        initial_state.cost = Solution::get_costs(&initial_state, &tops, &bottoms);
+        states.push(initial_state);
+        while !states.is_empty() {
+            let current_state = states.pop().unwrap();
+            for choice in 0..number_of_choices {
+                if !current_state.moves.contains(&choice) {
+                    let mut new_state = State {
+                        cost: 0,
+                        moves: current_state.moves.clone(),
+                    };
+                    new_state.moves.insert(choice);
+                    if Solution::completed(&new_state, &tops, &bottoms) {
+                        return new_state.moves.len() as i32;
+                    }
+                    new_state.cost = Solution::get_costs(&new_state, &tops, &bottoms);
+                    if new_state.cost < current_state.cost {
+                        states.push(new_state);
+                    }
+                }
+            }
+        }
+        -1
     }
 
-    fn same_elements(list: &[i32]) -> bool {
-        for elem in list.iter() {
-            if list[0] != *elem {
+    fn get_costs(state: &State, tops: &[i32], bottoms: &[i32]) -> usize {
+        let mut top_map = HashMap::<i32, i32>::new();
+        let mut bottom_map = HashMap::<i32, i32>::new();
+
+        for index in 0..tops.len() {
+            if state.moves.contains(&index) {
+                let top_count = top_map.entry(bottoms[index]).or_insert(0);
+                *top_count += 1;
+                let bottom_count = bottom_map.entry(tops[index]).or_insert(0);
+                *bottom_count += 1;
+            } else {
+                let top_count = top_map.entry(tops[index]).or_insert(0);
+                *top_count += 1;
+                let bottom_count = bottom_map.entry(bottoms[index]).or_insert(0);
+                *bottom_count += 1;
+            }
+        }
+
+        let top_most_occurences = *top_map.values().max().unwrap() as usize;
+        let bottom_most_occurences = *bottom_map.values().max().unwrap() as usize;
+
+        10000 * (tops.len() - usize::max(top_most_occurences, bottom_most_occurences))
+            + state.moves.len()
+    }
+
+    fn completed(state: &State, tops: &[i32], bottoms: &[i32]) -> bool {
+        let (mut top_complete, mut bottom_complete) = (true, true);
+        for index in 0..tops.len() {
+            if state.moves.contains(&index) {
+                if tops[0] != bottoms[index] {
+                    top_complete = false;
+                }
+                if bottoms[0] != tops[index] {
+                    bottom_complete = false;
+                }
+            } else {
+                if tops[0] != tops[index] {
+                    top_complete = false;
+                }
+                if bottoms[0] != bottoms[index] {
+                    bottom_complete = false;
+                }
+            }
+            if !top_complete && !bottom_complete {
                 return false;
             }
         }
         true
-    }
-
-    pub fn min_domino_rotations(tops: Vec<i32>, bottoms: Vec<i32>) -> i32 {
-        let number_of_choices = tops.len();
-        let mut states = VecDeque::<State>::new();
-        let initial_state = State {
-            tops,
-            bottoms,
-            flipped: HashSet::<usize>::new(),
-        };
-        states.push_back(initial_state);
-
-        while !states.is_empty() {
-            // Pop the current state from the front of the queue.
-            let current_state = states.pop_front().unwrap();
-            // Itterate over all possible moves
-            for choice in 0..number_of_choices {
-                // Check if a move should be made
-                if !current_state.flipped.contains(&choice) {
-                    // Generate a new state by applying the move
-                    let new_state = Solution::flip(&current_state, choice);
-
-                    // Check if we have reached completion
-                    if Solution::same_elements(&new_state.tops)
-                        || Solution::same_elements(&new_state.bottoms)
-                    {
-                        // Return the number of steps of the solution
-                        return new_state.flipped.len() as i32;
-                    }
-
-                    // Push the new state to the back of the queue
-                    states.push_back(new_state);
-                }
-            }
-        }
-        // After visiting all states, return -1
-        -1
     }
 }
 // @leetup=code
@@ -134,6 +172,34 @@ mod tests {
     fn example_2() {
         let tops = vec![3, 5, 1, 2, 3];
         let bottoms = vec![3, 6, 3, 3, 4];
+        assert_eq!(Solution::min_domino_rotations(tops, bottoms), -1)
+    }
+
+    #[test]
+    fn example_3() {
+        let tops = vec![1, 1, 1];
+        let bottoms = vec![1, 1, 1];
+        assert_eq!(Solution::min_domino_rotations(tops, bottoms), 0)
+    }
+
+    #[test]
+    fn example_4() {
+        let tops = vec![1, 0];
+        let bottoms = vec![1, 0];
+        assert_eq!(Solution::min_domino_rotations(tops, bottoms), -1)
+    }
+
+    #[test]
+    fn example_5() {
+        let tops = vec![1, 0];
+        let bottoms = vec![0, 1];
+        assert_eq!(Solution::min_domino_rotations(tops, bottoms), 1)
+    }
+
+    #[test]
+    fn example_6() {
+        let tops = vec![1, 3, 2, 3, 4, 1, 1, 4, 1];
+        let bottoms = vec![5, 1, 5, 5, 2, 3, 5, 3, 1];
         assert_eq!(Solution::min_domino_rotations(tops, bottoms), -1)
     }
 }
