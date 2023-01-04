d559c665cecdf26d16f7a993d4aa6d55f92e7aae
Floris Cornel <floris@cornel.email>
2023-01-04 10:40:24 +0000

    fix problem grep commit.sh

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add src
-PROBLEM=$(git status | grep problems | cut -d: -f2 | head -n 1 | cut -d '/' -f3 | cut -d '.' -f1)
+PROBLEM=$(git status | grep 'problems' | grep -v 'mod' | cut -d: -f2 | head -n 1 | cut -d '/' -f3 | cut -d '.' -f1)
 [ -z "$PROBLEM" ] && exit 1
 git fetch --all
 git pull && cargo check && cargo fmt && cargo clippy && cargo test && git commit -m "$PROBLEM" && git push
