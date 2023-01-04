e9822784afc8845fa650edce16a5d766ac071e78
Floris Cornel <floris@cornel.email>
2023-01-04 10:12:20 +0000

    fix commit.sh

--- commit.sh
+++ commit.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cargo check
-cargo fmt
-cargo clippy
-cargo test
-git add .
-git commit -m $(git status | grep "new file" | grep problems | cut -d: -f2 | head -n 1 | cut -d '/' -f3 | cut -d '.' -f1)
+git add src
+PROBLEM=$(git status | grep problems | cut -d: -f2 | head -n 1 | cut -d '/' -f3 | cut -d '.' -f1)
+[ -z "$PROBLEM" ] && exit 1
+git fetch --all
+git pull && cargo check && cargo fmt && cargo clippy && cargo test && git commit -m "$PROBLEM" && git push
